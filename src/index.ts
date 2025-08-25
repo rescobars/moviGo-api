731f0bb3f916218b384db842f55485f72ad5cd1f
Robin Escobar <rescobar@buildwithin.com>
2025-08-25 00:59:27 -0600

    add api

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:8081'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:8081', "https://manos-web-app-ae845d78f7b1.herokuapp.com"],
   credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
